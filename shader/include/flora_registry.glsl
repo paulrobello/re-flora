39d91be14fc5b074168f77736a2f24bcc92d95e8
danny <ruitian0716@gmail.com>
2025-11-26 00:35:57 +0800

    chore: formatting

--- shader/include/flora_registry.glsl
+++ shader/include/flora_registry.glsl
@@ -1,10 +1,10 @@
 #ifndef FLORA_REGISTRY_GLSL
 #define FLORA_REGISTRY_GLSL
 
-const uint MAX_FLORA_SPECIES   = 3;
-const uint FLORA_SPECIES_COUNT = 3;
-const uint FLORA_SPECIES_GRASS = 0;
-const uint FLORA_SPECIES_LAVENDER = 1;
+const uint MAX_FLORA_SPECIES         = 3;
+const uint FLORA_SPECIES_COUNT       = 3;
+const uint FLORA_SPECIES_GRASS       = 0;
+const uint FLORA_SPECIES_LAVENDER    = 1;
 const uint FLORA_SPECIES_EMBER_BLOOM = 2;
 
 #endif // FLORA_REGISTRY_GLSL
