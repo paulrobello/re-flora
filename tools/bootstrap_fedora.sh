4ec28b13a8013d02c207e35220ff16c1ce727ec9
terence <ruitian0716@gmail.com>
2026-01-05 01:12:49 +0800

    upgrade bootstrap code wip

---
+++ tools/bootstrap_fedora.sh
@@ -0,0 +1,6 @@
+# install cmake
+sudo dnf install cmake
+
+# install alsa-lib-devel for alsa.pc to work
+# <https://www.reddit.com/r/linuxquestions/comments/pwxa8u/libasound2dev_on_fedora/>
+sudo dnf install alsa-lib-devel
