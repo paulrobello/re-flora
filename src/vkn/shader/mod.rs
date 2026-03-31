10367a143f67ac03213afd291ccc53c6e2dd0d84
terence <ruitian0716@gmail.com>
2026-04-01 02:13:53 +0800

    remove old code
    w

--- src/vkn/shader/mod.rs
+++ src/vkn/shader/mod.rs
@@ -1,13 +1,5 @@
 mod struct_layout;
 pub use struct_layout::*;
 
-mod data_builder;
-#[allow(unused_imports)]
-pub use data_builder::*;
-
-mod data_reader;
-#[allow(unused_imports)]
-pub use data_reader::*;
-
 mod shader_module;
 pub use shader_module::*;
